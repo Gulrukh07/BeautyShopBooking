e692708a6b37088477605e478dcde85ffb6c6187
glrkh <gulrukhkhalilovna@gmail.com>
2025-10-27 11:22:18 +0500

    updates

--- root/settings.py
+++ root/settings.py
@@ -84,8 +84,8 @@ WSGI_APPLICATION = 'root.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': {
-        dj_database_url.config(default=os.getenv("DATABASE_URL"))    }
+    'default':
+        dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
