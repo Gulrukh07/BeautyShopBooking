a19d4c8d0eac06f93b7c0175c275c7e9b72a1457
glrkh <gulrukhkhalilovna@gmail.com>
2025-11-03 12:47:44 +0500

    updates

--- apps/serializers.py
+++ apps/serializers.py
@@ -9,7 +9,7 @@ from apps.models import User, Business, Appointment, Service, SubService, Notifi
 class UserModelSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = 'id', 'first_name', 'last_name', 'phone_number', 'password', 'avatar', 'role'
+        fields  = '__all__'
         read_only_fields = 'created_at', 'updated_at', 'date_joined'
 
     def validate_phone_number(self, value):
